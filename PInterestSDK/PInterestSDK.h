f64e6b71752b969f6f3ee6799b01cb7d2fd39058
Peyman Khanjan <p.khanjan@gmail.com>
2016-10-22 18:47:56 +0100

    Carthage support.

---
+++ PInterestSDK/PInterestSDK.h
@@ -0,0 +1,18 @@
+//
+//  PInterestSDK.h
+//  PInterestSDK
+//
+//  Created by Peyman Khanjan on 22/10/2016.
+//  Copyright © 2016 Snupps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <PinterestSDK/PDKBoard.h>
+#import <PinterestSDK/PDKCategories.h>
+#import <PinterestSDK/PDKClient.h>
+#import <PinterestSDK/PDKImageInfo.h>
+#import <PinterestSDK/PDKInterest.h>
+#import <PinterestSDK/PDKModelObject.h>
+#import <PinterestSDK/PDKResponseObject.h>
+#import <PinterestSDK/PDKUser.h>
